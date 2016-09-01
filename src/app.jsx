d0d030ea86b21911d4091762fb0247be5e41c344
Yuriy Naidyon <ynaidyon@twitter.com>
2016-08-31 21:08:39 -0700

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+// import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const message = 'Here we go...';
+
+ReactDOM.render(
+    <h1>{message}</h1>,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        path: './build',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }
+};
